50d81cb2afc7c63ca2403139d1beb7bd985fd4df
Oleg Valter <o.a.philippov@gmail.com>
2021-05-27 16:48:47 +0300

    Installed header generator, generated headers

---
+++ dist/headers.js
@@ -0,0 +1,10 @@
+
+// ==UserScript==
+// @author Oleg Valter
+// @description Chat experience improvements
+// @homepage https://github.com/userscripters/template#readme
+// @name chat-overcharge
+// @source git+https://github.com/userscripters/template.git
+// @supportURL https://github.com/userscripters/template/issues
+// @version 1.4.2
+// ==/UserScript==

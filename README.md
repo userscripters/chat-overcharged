6109139eda14cdd42aebad2ce6ab3431ad83257c
Oleg Valter <o.a.philippov@gmail.com>
2021-07-26 12:55:33 +0300

    FIX: repo references point to template repo

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 
 # About
 
-| Author       | Oleg Valter |
+| Author       | Oleg Valter<br>email: [oleg.a.valter@gmail.com](mailto:oleg.a.valter@gmail.com) |
 | :----------- | :----------------------- |
-| Name         | Chat-overcharge    |
+| Name         | @userscripters/chat-overcharged    |
 | Description  | Chat experience improvements           |
 | License      | [GPL-3.0-or-later](https://spdx.org/licenses/GPL-3.0-or-later)                 |
 | Version      | 1.5.1               |
 
 # Support
 
-Bug reports for the project should be [submitted here](https://github.com/userscripters/template/issues).
+Bug reports for the project should be [submitted here](https://github.com/userscripters/chat-overcharged/issues).
 <br>Before adding a new one, please check if it hasn't been raised before.
   
--- dist/legacy/index.min.js
+++ dist/legacy/index.min.js
@@ -1,13 +1,14 @@
 // ==UserScript==
-// @author          Oleg Valter
+// @author          Oleg Valter <oleg.a.valter@gmail.com>
 // @description     Chat experience improvements
 // @grant           none
-// @homepage        https://github.com/userscripters/template#readme
+// @homepage        https://github.com/userscripters/chat-overcharged#readme
 // @match           *://chat.stackexchange.com/*
 // @match           *://chat.stackoverflow.com/*
-// @name            chat-overcharge
-// @source          git+https://github.com/userscripters/template.git
-// @supportURL      https://github.com/userscripters/template/issues
+// @name            chat-overcharged
+// @namespace       userscripters
+// @source          git+https://github.com/userscripters/chat-overcharged.git
+// @supportURL      https://github.com/userscripters/chat-overcharged/issues
 // @version         1.5.1
 // ==/UserScript==
 

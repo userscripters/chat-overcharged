2f8b6a10301d2c0084103ead4c1bde4973c458df
Oleg Valter <o.a.philippov@gmail.com>
2021-07-27 12:16:13 +0300

    1.6.1

--- dist/modern/index.js
+++ dist/modern/index.js
@@ -9,7 +9,7 @@
 // @namespace       userscripters
 // @source          git+https://github.com/userscripters/chat-overcharged.git
 // @supportURL      https://github.com/userscripters/chat-overcharged/issues
-// @version         1.6.0
+// @version         1.6.1
 // ==/UserScript==
 
 "use strict";

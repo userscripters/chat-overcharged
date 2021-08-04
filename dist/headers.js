fc08e5b09e60e070b41b2652d494df951408054b
Oleg Valter <o.a.philippov@gmail.com>
2021-08-04 19:04:52 +0300

    1.8.0

--- dist/headers.js
+++ dist/headers.js
@@ -9,5 +9,5 @@
 // @namespace       userscripters
 // @source          git+https://github.com/userscripters/chat-overcharged.git
 // @supportURL      https://github.com/userscripters/chat-overcharged/issues
-// @version         1.7.0
+// @version         1.8.0
 // ==/UserScript==
